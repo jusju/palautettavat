d52c7186fcb559886920d86544b6b39a07637074
Juslin Jukka <jusju@haaga-helia.fi>
2020-11-15 10:14:12 +0200

    dao implementation refactoring for item

--- src/viikko11/JDBCShoppingListItemDao.java
+++ src/viikko11/JDBCShoppingListItemDao.java
@@ -1,5 +1,11 @@
 package viikko11;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JDBCShoppingListItemDao implements ShoppingListItemDao {
@@ -8,22 +14,123 @@ public class JDBCShoppingListItemDao implements ShoppingListItemDao {
 	
     @Override
     public List<ShoppingListItem> getAllItems() {
-        return null;
+    	List<ShoppingListItem> items = new ArrayList<ShoppingListItem>();
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection connection = DriverManager.getConnection(URL);
+			PreparedStatement statement = connection.prepareStatement("SELECT * FROM ShoppingListItem");
+			ResultSet results = statement.executeQuery();
+			
+			while (results.next()) {
+				ShoppingListItem item = new ShoppingListItem();
+				item.setId(results.getInt("id"));
+				item.setOstos(results.getString("title"));
+				items.add(item);
+			}
+			results.close();
+			statement.close();
+			connection.close();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return items;
     }
 
     @Override
     public ShoppingListItem getItem(long id) {
-        return null;
+		ShoppingListItem item = new ShoppingListItem();
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection connection = DriverManager.getConnection(URL);
+			PreparedStatement statement = connection.prepareStatement("SELECT * FROM ShoppingListItem");
+			ResultSet results = statement.executeQuery();
+
+			while (results.next()) {
+				if(id == results.getInt("id")) {
+					item.setId(results.getInt("id"));
+					item.setOstos(results.getString("title"));
+				}
+			}
+			results.close();
+			statement.close();
+			connection.close();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return item;
     }
 
     @Override
     public boolean addItem(ShoppingListItem newItem) {
-        return false;
+		
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection connection = DriverManager.getConnection(URL);
+			PreparedStatement statement = connection.prepareStatement("INSERT INTO ShoppingListItem(title) VALUES (?)",
+					Statement.RETURN_GENERATED_KEYS);
+			
+			statement.setString(1, newItem.getOstos());
+			statement.executeUpdate();
+			
+			ResultSet keys = statement.getGeneratedKeys();
+			if(keys.next()) {
+				long id = keys.getLong(1);
+				
+			}
+			keys.close();
+			statement.close();
+			connection.close();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
     }
 
     @Override
     public boolean removeItem(ShoppingListItem item) {
-        return false;
+    	int deletedId = 0;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection connection = DriverManager.getConnection(URL);
+			PreparedStatement statement = connection.prepareStatement("SELECT * FROM ShoppingListItem WHERE title LIKE(?)");
+			statement.setString(1, item.getOstos());
+			ResultSet results = statement.executeQuery();
+
+			deletedId = results.getInt("id");
+
+			results.close();
+			statement.close();
+			connection.close();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection connection = DriverManager.getConnection(URL);
+			PreparedStatement statement = connection.prepareStatement("DELETE FROM ShoppingListItem WHERE id = ?");
+			statement.setInt(1, deletedId);
+			statement.executeUpdate();
+
+			statement.close();
+			connection.close();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}    	
+		return true;
     }
 
 }
